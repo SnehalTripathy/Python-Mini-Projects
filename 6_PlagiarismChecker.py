c376b4473d3c84dc49416bc005581e91943995d9
SnehalTripathy <snehaltripathy620@gmail.com>
2025-07-20 21:13:11 +0530

    Updated 6_PlagiarismChecker.py

--- 6_PlagiarismChecker.py
+++ 6_PlagiarismChecker.py
@@ -4,4 +4,4 @@ with open('demo1.txt') as one_file, open('demo2.txt') as two_file:
     data_file1 = one_file.read()
     data_file2 = two_file.read()
     matches = SequenceMatcher(None, data_file1, data_file2).ratio()
-    print(f"The plagiarised content is {matches * 100:.2f}% similar.")
+    print(f"The plagiarised content found is {matches * 100:.2f}% similar.")
