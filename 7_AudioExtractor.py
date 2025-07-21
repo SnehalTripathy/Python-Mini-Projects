80667ad8c603a0d925876fdad218a042176ca5a2
SnehalTripathy <snehaltripathy620@gmail.com>
2025-07-21 10:38:00 +0530

    Modified 7_AudioExtractor.py

--- 7_AudioExtractor.py
+++ 7_AudioExtractor.py
@@ -3,3 +3,4 @@ import moviepy as mp
 cvt_video = mp.VideoFileClip("outro.mov")
 ext_audio = cvt_video.audio
 ext_audio.write_audiofile("reel_extracted_audio2.mp3")
+print("Audio extraction complete.")
