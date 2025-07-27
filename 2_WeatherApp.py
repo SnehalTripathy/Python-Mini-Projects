7d883df8fc640ea7a60b7772467b7420086b0cfd
SnehalTripathy <snehaltripathy620@gmail.com>
2025-07-27 19:38:59 +0530

    Updated 2_WeatherApp.py

--- 2_WeatherApp.py
+++ 2_WeatherApp.py
@@ -5,3 +5,4 @@ city = input("Enter the name of the city: ")
 url = f"https://api.weatherapi.com/v1/current.json?key=a97c8ffacb42406eb6b153706222401&q={city}"
 r = requests.get(url)
 print(r.text)
+print("Weather data fetched successfully.")
